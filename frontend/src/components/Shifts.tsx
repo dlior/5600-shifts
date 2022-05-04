1f81671824efe1fcb08ecaa3f43011c5804a7c8d
lior <lior.degu@gmail.com>
2022-05-04 07:15:12 +0300

    add global style

--- frontend/src/components/Shifts.tsx
+++ frontend/src/components/Shifts.tsx
@@ -10,6 +10,8 @@ import {
   Tr,
 } from '@chakra-ui/react';
 
+import '../index.css';
+
 const Shifts = () => {
   return (
     <TableContainer w="80vw" mx="auto" mt={16}>
@@ -17,21 +19,21 @@ const Shifts = () => {
         <Thead>
           <Tr>
             <Th>Employee Name</Th>
-            <Th>Sun</Th>
-            <Th>Mon</Th>
-            <Th>Tue</Th>
-            <Th>Wed</Th>
-            <Th>Thu</Th>
+            <Th className="calendar-day">Sun</Th>
+            <Th className="calendar-day">Mon</Th>
+            <Th className="calendar-day">Tue</Th>
+            <Th className="calendar-day">Wed</Th>
+            <Th className="calendar-day">Thu</Th>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
             <Td>Lior Degu</Td>
-            <Td>-</Td>
-            <Td>07:30 - 19:30</Td>
-            <Td>07:30 - 19:30</Td>
-            <Td>07:30 - 19:30</Td>
-            <Td>-</Td>
+            <Td className="calendar-hours">-</Td>
+            <Td className="calendar-hours">07:30 - 19:30</Td>
+            <Td className="calendar-hours">07:30 - 19:30</Td>
+            <Td className="calendar-hours">07:30 - 19:30</Td>
+            <Td className="calendar-hours">-</Td>
           </Tr>
         </Tbody>
         <Tfoot>
---
+++ frontend/src/pages/Calendar.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+const Calendar = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Calendar;
