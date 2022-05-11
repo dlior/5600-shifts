4c4755f3b23763d668c3839fbad1ad6ebe7d7c0e
lior <lior.degu@gmail.com>
2022-05-11 10:46:57 +0300

    add utils file with dates, week and working days

---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,14 @@
+export const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+export const days = Array.from(Array(31).keys());
+export const currentDay = new Date().getDate();
+export const currentMonth = new Date().getMonth() + 1;
+export const currentYear = new Date().getFullYear();
+
+export const workDays = ['sun', 'mon', 'tue', 'wed', 'thu'];
+export const workDaysColors = [
+  'teal.500',
+  'blue.500',
+  'cyan.500',
+  'purple.500',
+  'pink.500',
+];
